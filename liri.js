421adf81157d7beec4c418b31a3d408327b6be53
Lotus Bug <torresbrittany1994@gmail.com>
2019-09-01 14:05:32 -0700

    spotify config

--- liri.js
+++ liri.js
@@ -1,1 +1,20 @@
+var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
+
 require("dotenv").config();
+ 
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }
+, function(err, data) {
+  if (err) {
+    console.log('Error occurred: ' + err);
+    return;
+  }
+ 
+console.log(data); 
+});
